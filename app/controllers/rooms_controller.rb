87f3e54ebcdb861488ad627e09fbb4e7ce306d8d
jorgegoco <jorgegoco70@gmail.com>
2023-07-02 21:56:03 +0200

    Add show method for rooms controller

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -4,8 +4,21 @@ class RoomsController < ApplicationController
   def index
     @room = Room.new
     @rooms = Room.public_rooms
-    @single_room = Room.find(params[:id])
     @users = User.all_except(current_user)
+    render 'index'
+  end
+
+  def show
+    @single_room = Room.find(params[:id])
+
+    @room = Room.new
+    @rooms = Room.public_rooms
+
+    @message = Message.new
+    @messages = @single_room.messages.order(created_at: :asc)
+
+    @users = User.all_except(current_user)
+    render 'index'
   end
 
   def create
