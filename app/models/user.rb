14bf774eddae686db770d4c7ec2babef1f70fc21
jorgegoco <jorgegoco70@gmail.com>
2023-07-05 20:03:04 +0200

    Add default avatar, in case in model

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,23 @@ class User < ApplicationRecord
   scope :all_except, ->(user) { where.not(id: user) }
   after_create_commit { broadcast_append_to 'users' }
   has_many :messages
+  has_one_attached :avatar
+
+  after_commit :add_default_avatar, on: %i[create update]
+
+  private
+
+  def add_default_avatar
+    unless avatar.attached?
+      avatar.attach(
+        io: File.open(
+          Rails.root.join(
+            'app', 'assets', 'images', 'default_profile.jpg'
+          )
+        ),
+        filename: 'default_profile.jpg',
+        content_type: 'image/png'
+      )
+    end
+  end
 end
