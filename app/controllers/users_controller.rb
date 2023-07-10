71919ac69e05b2e2940ac59e50dc5df0b2d08c37
jorgegoco <jorgegoco70@gmail.com>
2023-07-10 23:18:22 +0200

    Add Stimulus JS scroll controller, and fix views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,7 +13,7 @@ class UsersController < ApplicationController
     pagy_messages = @single_room.messages.order(created_at: :desc)
     @pagy, messages = pagy(pagy_messages, items: 10)
     @messages = messages.reverse
-    
+
     render 'rooms/index'
   end
 
